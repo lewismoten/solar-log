61246609b3b830ca4a1ba5717fefb8d4830058e4
Lewis Moten <lewismoten@gmail.com>
2017-10-04 22:01:01 -0500

    Graph csv data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # solar-log
+
+This is just a little project to parse a CSV file downloaded from EPEVER eLOG01 Record Accessory Adapter For EPEVER Tracer Series eTracer Series MPPT Solar Charge Controller
+
+![](screenshot.png)
---
+++ index.js
@@ -0,0 +1,263 @@
+var log;
+
+/*
+"Record Num.": "287",
+    "Array Current(A) ": "0.00",
+    "Array Voltage(V)": "3.78",
+    "Array Power(W)": "0.00",
+    "Load Current(A)": "0.00",
+    "Load Voltage(V)": "13.27",
+    "Load Power(W)": "0.00",
+    "Battery Current(A)": "0.00",
+    "Battery Voltage(V)": "13.27",
+    "Battery Temp.(℃)": "25.00",
+    "Battery SOC(%)": "80",
+    "Battery Max. Voltage(V)": "14.39",
+    "Battery Min. Voltage(V)": "13.2",
+    "Array Status": "CutOut",
+    "Charging Status": "NotCharging",
+    "Battery Status": "Normal",
+    "Load Status": "On",
+    "Device Status": "Normal",
+    "Daily Energy Consumed(kWh)": "0.00",
+    "Monthly Energy Consumed(kWh)": "0.00",
+    "Annual Energy Consumed(kWh)": "0.01",
+    "Total Energy Consumed(kWh)": "0.01",
+    "Daily Energy Generated(kWh)": "0.11",
+    "Monthly Energy Generated(kWh)": "0.49",
+    "Annual Energy Generated(kWh)": "1.15",
+    "Total Energy Generated(kWh)": "2.42"
+*/
+$(document).ready(function() {
+  $.ajax("solar3.csv")
+    .done(function(data) {
+       var lines = data.split("\r\n");
+       var headers = lines.shift().split(",").map(function(header){
+         return header.trim();
+       });
+       headers.push("timestamp");
+       lines.pop(); // last line has nada
+
+      // Convert to object
+      log = lines.map(function(line) {
+        var record = {};
+        var values = line.split(",");
+        headers.forEach(function(header, i) {
+          record[header] = values[i];
+
+          if(header === "timestamp") {
+            record[header] = tsAsDate(values[i], values[0]);
+          }
+        });
+
+        return record;
+      });
+      //log.sort(sortLog);
+
+      google.charts.load("current", {packages: ["corechart", "line", "timeline"]});
+      google.charts.setOnLoadCallback(drawBasic);
+
+      function drawBasic() {
+        drawAmps();
+        drawVolts();
+        drawWatts();
+        drawBattery();
+        drawStatus();
+      }
+    });
+});
+
+function drawAmps() {
+  var data = new google.visualization.DataTable();
+  data.addColumn("number", "id");
+  data.addColumn("number", "Solar");
+  data.addColumn("number", "Load");
+  data.addColumn("number", "Battery");
+  data.addRows(log.map(function(line) {
+    return [
+      Number(line["Record Num."]),
+      Number(line["Array Current(A)"]),
+      Number(line["Load Current(A)"]),
+      Number(line["Battery Current(A)"])
+    ];
+  }));
+
+  var options = {
+    hAxis: {
+      title: 'Record'
+    },
+    vAxis: {
+      title: 'Amps'
+    }
+  };
+  var chart = new google.visualization.LineChart(document.getElementById('chart_amps'));
+  chart.draw(data, options);
+}
+
+function drawVolts() {
+  var data = new google.visualization.DataTable();
+  data.addColumn("number", "id");
+  data.addColumn("number", "Solar");
+  data.addColumn("number", "Load");
+  data.addColumn("number", "Battery");
+  data.addRows(log.map(function(line) {
+    return [
+      Number(line["Record Num."]),
+      Number(line["Array Voltage(V)"]),
+      Number(line["Load Voltage(V)"]),
+      Number(line["Battery Voltage(V)"])
+    ];
+  }));
+
+  var options = {
+    hAxis: {
+      title: 'Record'
+    },
+    vAxis: {
+      title: 'Voltage'
+    }
+  };
+  var chart = new google.visualization.LineChart(document.getElementById('chart_volts'));
+  chart.draw(data, options);
+}
+
+function drawWatts() {
+  var data = new google.visualization.DataTable();
+  data.addColumn("number", "id");
+  data.addColumn("number", "Solar");
+  data.addColumn("number", "Load");
+  data.addColumn("number", "Battery");
+  data.addRows(log.map(function(line) {
+    return [
+      Number(line["Record Num."]),
+      Number(line["Array Power(W)"]),
+      Number(line["Load Power(W)"]),
+      Number(line["Battery Power(W)"])
+    ];
+  }));
+
+  var options = {
+    hAxis: {
+      title: 'Record'
+    },
+    vAxis: {
+      title: 'Watts'
+    }
+  };
+  var chart = new google.visualization.LineChart(document.getElementById('chart_watts'));
+  chart.draw(data, options);
+}
+
+function drawBattery() {
+  var data = new google.visualization.DataTable();
+  data.addColumn("number", "id");
+  data.addColumn("number", "Minimum");
+  data.addColumn("number", "Battery");
+  data.addColumn("number", "Maximum");
+  data.addRows(log.map(function(line) {
+    return [
+      Number(line["Record Num."]),
+      Number(line["Battery Min. Voltage(V)"]),
+      Number(line["Battery Voltage(V)"]),
+      Number(line["Battery Max. Voltage(V)"])
+    ];
+  }));
+
+  var options = {
+    hAxis: {
+      title: 'Record'
+    },
+    vAxis: {
+      title: 'Voltage'
+    }
+  };
+  var chart = new google.visualization.LineChart(document.getElementById('chart_battery'));
+  chart.draw(data, options);
+}
+
+function drawStatus() {
+  var data = new google.visualization.DataTable();
+  data.addColumn({ type: 'string', id: 'Type' });
+  data.addColumn({ type: 'string', id: 'Name' });
+  data.addColumn({ type: 'date', id: 'Start' });
+  data.addColumn({ type: 'date', id: 'End' });
+  data.addRows(
+    [].concat(
+       getEvents("Array", "Array Status"),
+       getEvents("Charging", "Charging Status"),
+       getEvents("Battery", "Battery Status"),
+       getEvents("Device", "Device Status")
+     )
+  );
+
+   function getEvents(type, name) {
+     var rows = [];
+     var state = [];
+     log.map(function(line, i) {
+       var timestamp = line["timestamp"];
+
+       if (line[name] !== state[1]) {
+         state = [type, line[name], timestamp, timestamp];
+         rows.push(state);
+       } else {
+         if (timestamp > state[2]) {
+           state[3] = timestamp;
+         } else {
+           console.log(type, i, "warning - end date is wrong!");
+         }
+       }
+     });
+     console.log(type, rows);
+     return rows;
+   }
+
+  var options = {
+    hAxis: {
+      title: 'Record'
+    },
+    vAxis: {
+      title: 'Voltage'
+    }
+  };
+  var chart = new google.visualization.Timeline(document.getElementById('chart_status'));
+  chart.draw(data, options);
+}
+
+function tsAsDate(ts, id) {
+  if(ts === "" || typeof ts === "undefined") {
+    // older entries didn't have dates - oops
+    return new Date(0, 0, 0, 0, id * 10, 0);
+  }
+  // 2017/2/14  12:11:0
+  var parts = ts.split("  ");
+  var d = parts[0].split("/");
+  var t = parts[1].split(":");
+  var year = Number(d[0]);
+  var month = Number(d[1]);
+  var day = Number(d[2]);
+  var hour = Number(t[0]);
+  var min = Number(t[1]);
+  var sec = Number(t[2]);
+  var date = new Date(year, month - 1, day, hour, min, sec);
+  if (id < 66) {
+    // for some reson day of month did not roll over
+    date.setDate(date.getDate()-1);
+  }
+  return date;
+}
+
+function sortLog(a, b) {
+  var aa = a.timestamp;
+  var bb = b.timestamp;
+  if (aa < bb) {
+    return - 1;
+  } else if (aa > bb) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+function getValue(line, name) {
+  var i = headers.indexOf(name);
+  return Number(line.split(",")[i]);
+}
