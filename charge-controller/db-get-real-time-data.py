b88c0fd0682df50849198082c23b43c0f650dbc7
Lewis Moten <lewismoten@gmail.com>
2017-10-30 17:40:42 -0500

    reduce file size of real-time-data results

--- charge-controller/db-get-real-time-data.py
+++ charge-controller/db-get-real-time-data.py
@@ -37,24 +37,24 @@ try:
 
     # Get a brief history...
     # Get {maxRecords} records representing the past {durationMiutes} minutes, grouped by {groupSeconds}
-    maxRecords = 100
+    maxRecords = 60
     durationMinutes = 60
     groupSeconds = (durationMinutes * 60) / (maxRecords - 1)
 
     sql = """
     SELECT
         DATE_FORMAT(FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(create_date)/{groupSeconds}) * {groupSeconds}), '%Y-%m-%d %H:%i:%s'),
-        AVG(rt_input_v),
-        AVG(rt_input_a),
-        AVG(rt_input_w),
-        AVG(rt_battery_v),
-        AVG(rt_battery_a),
-        AVG(rt_battery_w),
-        AVG(rt_battery_soc) * 100,
-        (AVG(rt_battery_temp) * (9/5)) + 32,
-        AVG(rt_load_v),
-        AVG(rt_load_a),
-        AVG(rt_load_w)
+        TRUNCATE(AVG(rt_input_v), {decimals}),
+        TRUNCATE(AVG(rt_input_a), {decimals}),
+        TRUNCATE(AVG(rt_input_w), {decimals}),
+        TRUNCATE(AVG(rt_battery_v), {decimals}),
+        TRUNCATE(AVG(rt_battery_a), {decimals}),
+        TRUNCATE(AVG(rt_battery_w), {decimals}),
+        TRUNCATE(AVG(rt_battery_soc) * 100, {decimals}),
+        TRUNCATE((AVG(rt_battery_temp) * (9/5)) + 32, {decimals}),
+        TRUNCATE(AVG(rt_load_v), {decimals}),
+        TRUNCATE(AVG(rt_load_a), {decimals}),
+        TRUNCATE(AVG(rt_load_w), {decimals})
     FROM
         controller_real_time_data
     WHERE
@@ -63,7 +63,7 @@ try:
         FLOOR(UNIX_TIMESTAMP(create_date)/{groupSeconds})
     ORDER BY create_date;
     """
-    c.execute(sql.format(**{"groupSeconds": groupSeconds, "duration": durationMinutes}))
+    c.execute(sql.format(**{"groupSeconds": groupSeconds, "duration": durationMinutes, "decimals": 2}))
     result["hour_fields"] = [
         "create_date",
         "rt_input_v",
