bfcb03b2d742798eb40a097c216b3e64581d3613
Lewis Moten <lewismoten@gmail.com>
2018-01-04 21:54:56 -0600

    show 4 hours of data

--- charge-controller/db-get-real-time-data.py
+++ charge-controller/db-get-real-time-data.py
@@ -70,7 +70,7 @@ try:
     FROM
         controller_real_time_data
     WHERE
-        create_date >= DATE_SUB(now(), INTERVAL 60 MINUTE)
+        create_date >= DATE_SUB(now(), INTERVAL 60 * 4 MINUTE)
     """
     # sql = """
     # SELECT
