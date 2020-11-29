2640f71b977327b0941ddddd7c25dc29740697fd
Lewis Moten <lewismoten@gmail.com>
2020-11-29 12:52:20 -0500

    print all

--- modbus/print_all.py
+++ modbus/print_all.py
@@ -1,14 +1,105 @@
-import logging
-logging.basicConfig()
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+#!/usr/bin/python
+import json
+from common import *
 
-import print_coils
-import print_discrete_input
+with open("schema.json", "r") as f:
+  schema = json.load(f)
 
-import print_device
-import print_ratings
-import print_data
-import print_status
-import print_statistics
-import print_settings
+def readInfo(info):
+    id = info["id"]
+    result = client.execute(ReadDeviceInformationRequest(id, unit=schema["device"]["unit"]))
+    if isinstance(result, Exception):
+        o = {"id": id, "error": True}
+    else:
+        if result.function_code < 0x80:
+            def asText(index):
+                return {"index": index, "ascii": result.information[index].decode("ascii")}
+            o = {"id": id, "data": list(map(asText, result.information))}
+        else:
+            o = {"id": id, "error": True}
+    return o
+
+def readCoil(addressInfo):
+    id = addressInfo["id"]
+    result = client.read_coils(id, 1, unit=schema["device"]["unit"])
+    if isinstance(result, Exception):
+        o = {"id": id, "error": True}
+    else:
+        if result.function_code < 0x80:
+            o = {"id": id, "data": list(result.bits)}
+        else:
+            o = {"id": id, "error": True}
+    return o
+
+def readDiscreteInput(addressInfo):
+    id = addressInfo["id"]
+    result = client.read_discrete_inputs(id, 1, unit=schema["device"]["unit"])
+    if isinstance(result, Exception):
+        o = {"id": id, "error": True}
+    else:
+        if result.function_code < 0x80:
+            o = {"id": id, "data": list(result.bits)}
+        else:
+            o = {"id": id, "error": True}
+    return o
+
+def readHoldingRegisters(addressInfo):
+    id = addressInfo["id"]
+    size = addressInfo["size"]
+    result = client.read_holding_registers(id, size, unit=schema["device"]["unit"])
+    if isinstance(result, Exception):
+        o = {"id": id, "error": True}
+    else:
+        if result.function_code < 0x80:
+            o = {"id": id, "data": list(result.registers)}
+        else:
+            o = {"id": id, "error": True}
+    return o
+
+def readInputRegister(addressInfo):
+    id = addressInfo["id"]
+    size = addressInfo["size"]
+    result = client.read_input_registers(id, size, unit=schema["device"]["unit"])
+    if isinstance(result, Exception):
+        o = {"id": id, "error": True}
+    else:
+        if result.function_code < 0x80:
+            o = {"id": id, "data": list(result.registers)}
+        else:
+            o = {"id": id, "error": True}
+    return o
+
+def asInfoWithData(id):
+    return readInfo(schema["infoById"][str(id)]);
+
+def asInputRegisterWithData(id):
+    return readInputRegister(schema["addressById"][str(id)]);
+
+def asHoldingRegistersWithData(id):
+    return readHoldingRegisters(schema["addressById"][str(id)]);
+
+def asDiscreteInputWithData(id):
+    return readDiscreteInput(schema["addressById"][str(id)]);
+
+def asCoilWithData(id):
+    return readCoil(schema["addressById"][str(id)]);
+
+client = getClient()
+connected = client.connect()
+if connected:
+    info = map(asInfoWithData, schema["allInfoIds"])
+    a = map(asCoilWithData, schema["addressCoilIds"])
+    b = map(asDiscreteInputWithData, schema["addressDiscreteInputIds"])
+    c = map(asHoldingRegistersWithData, schema["addressHoldingRegisterIds"])
+    d = map(asInputRegisterWithData, schema["addressInputRegisterIds"])
+    out = {
+        "info": list(info),
+        "address": list(a) + list(b) + list(c) + list(d)
+    }
+    client.close()
+else:
+    out = {"error": "unable to connect", "connected": connected}
+
+print("Content-Type: application/json")
+print()
+print(json.dumps(out, indent=2))
