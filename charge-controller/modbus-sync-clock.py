7cd2c8c9604920685d4bb091a9c1733bc2a893eb
Lewis Moten <lewismoten@gmail.com>
2021-09-19 19:06:10 -0400

    synchronize clocks on multiple units

--- charge-controller/modbus-sync-clock.py
+++ charge-controller/modbus-sync-clock.py
@@ -8,64 +8,24 @@ from datetime import datetime
 # make sure you are connected...
 # dmesg | grep ttyUSB
 
-client = ModbusClient(
-    method = "rtu",
-    port = "/dev/ttyUSB0",
-    baudrate = 115200,
-    timeout = 1
-)
 
-if client.connect():
-    print("Reading Real-Time Clock from device...")
-    result = client.read_holding_registers(0x9013, 3, unit=1)
-    if isinstance(result, Exception):
-        print("got exception")
-        print(result)
-    elif result.function_code >= 0x80:
-        print("got bad function code")
-        print(result.function_code)
-        print(result)
-    else:
-        print("... Date({}, {}, {}, {}, {}, {})".format(
-            2000 + (result.registers[2] >> 8),
-            result.registers[2] & 0xFF,
-            result.registers[1] >> 8,
-            result.registers[1] & 0xFF,
-            result.registers[0] >> 8,
-            result.registers[0] & 0xFF
-        ))
+units = [1, 2];
 
-        print("Today is ...")
-        # Local time
-        now = datetime.now()
-        print("... {}".format(now))
-        newData = [0,0,0]
-        newData[2] = ((now.year - 2000) << 8) + now.month
-        newData[1] = (now.day << 8) + now.hour
-        newData[0] = (now.minute << 8) + now.second
-        print("... Date({}, {}, {}, {}, {}, {})".format(
-            2000 + (newData[2] >> 8),
-            newData[2] & 0xFF,
-            newData[1] >> 8,
-            newData[1] & 0xFF,
-            newData[0] >> 8,
-            newData[0] & 0xFF
-        ))
+for UNIT in units:
 
-        print("Updating Device RTC...")
-        result = client.write_registers(0x9013, newData, unit=1)
-        if isinstance(result, Exception):
-            print("got exception")
-            print(result)
-        elif result.function_code >= 0x80:
-            print("got bad function code")
-            print(result.function_code)
-            print(result)
-        else:
-            print("... Wrote data!")
+    print("----[ UNIT:", UNIT,"]----------");
 
-        print("Verifying...")
-        result = client.read_holding_registers(0x9013, 3, unit=1)
+    client = ModbusClient(
+        method = "rtu",
+        port = "/dev/ttyUSB0",
+        baudrate = 115200,
+        timeout = 1
+    )
+
+
+    if client.connect():
+        print("Reading Real-Time Clock from device...")
+        result = client.read_holding_registers(0x9013, 3, unit=UNIT)
         if isinstance(result, Exception):
             print("got exception")
             print(result)
@@ -83,8 +43,56 @@ if client.connect():
                 result.registers[0] & 0xFF
             ))
 
+            print("Today is ...")
+            # Local time
+            now = datetime.now()
+            print("... {}".format(now))
+            newData = [0,0,0]
+            newData[2] = ((now.year - 2000) << 8) + now.month
+            newData[1] = (now.day << 8) + now.hour
+            newData[0] = (now.minute << 8) + now.second
+            print("... Date({}, {}, {}, {}, {}, {})".format(
+                2000 + (newData[2] >> 8),
+                newData[2] & 0xFF,
+                newData[1] >> 8,
+                newData[1] & 0xFF,
+                newData[0] >> 8,
+                newData[0] & 0xFF
+            ))
 
-    client.close()
-else:
-    print("unable to connect")
-print("done")
+            print("Updating Device RTC...")
+            result = client.write_registers(0x9013, newData, unit=UNIT)
+            if isinstance(result, Exception):
+                print("got exception")
+                print(result)
+            elif result.function_code >= 0x80:
+                print("got bad function code")
+                print(result.function_code)
+                print(result)
+            else:
+                print("... RTC clock update command successful")
+
+            print("Verifying...")
+            result = client.read_holding_registers(0x9013, 3, unit=UNIT)
+            if isinstance(result, Exception):
+                print("got exception")
+                print(result)
+            elif result.function_code >= 0x80:
+                print("got bad function code")
+                print(result.function_code)
+                print(result)
+            else:
+                print("... Date({}, {}, {}, {}, {}, {})".format(
+                    2000 + (result.registers[2] >> 8),
+                    result.registers[2] & 0xFF,
+                    result.registers[1] >> 8,
+                    result.registers[1] & 0xFF,
+                    result.registers[0] >> 8,
+                    result.registers[0] & 0xFF
+                ))
+
+
+        client.close()
+    else:
+        print("unable to connect")
+print("---- [ done ] --------")
